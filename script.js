8331b564f66fb81bacb130820fe60054501ac327
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-07-28 17:45:49 -0300

    feat: add example to call loader modal

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const isLoading = (close = false) => {
-    if (close) {
-
-        $.magnificPopup.open({
-            modal: true,
-            items: {
-                type: 'inline',
-                src: '#modal'
-            },
-            callbacks: {
-                open: function() {
-                    $('#modal').removeClass('hidden');
-                }
-            }
-        }, 0);
-        return;
-
-    }
-
-    $.magnificPopup.close({
+  if (close) {
+    $.magnificPopup.open(
+      {
         modal: true,
         items: {
-            type: 'inline',
-            src: '#modal'
+          type: 'inline',
+          src: '#loaderModal',
         },
         callbacks: {
-            close: function() {
-                $('#modal').addClass('hidden');
-            }
-        }
-    }, 0);
+          open: function () {
+            $('#loaderModal').removeClass('hidden');
+          },
+        },
+      },
+      0
+    );
+    return;
+  }
 
-}
+  $.magnificPopup.close(
+    {
+      modal: true,
+      items: {
+        type: 'inline',
+        src: '#loaderModal',
+      },
+      callbacks: {
+        close: function () {
+          $('#loaderModal').addClass('hidden');
+        },
+      },
+    },
+    0
+  );
+};
+
+handleExample = () => {
+  isLoading(true);
+  setTimeout(() => {
+    window.location.reload();
+  }, 2500);
+};
